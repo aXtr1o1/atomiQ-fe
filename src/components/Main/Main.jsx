23a96316c80f270f54c29a1340dd2630082ed229
Antric K Nayakam <uiantric06@gmail.com>
2024-12-04 09:02:38 +0000

    Connected to Gemini api for time being and added the animation and ReadMe support

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,51 +1,115 @@
-import React from 'react'
-import './Main.css'
-import { assets } from '../../assets/assets'
+import React, {useContext, useEffect, useRef, useState} from 'react';
+import './Main.css';
+import {assets} from "../../assets/assets.js";
+import {Context} from "../../context/context.jsx";
 
 const Main = () => {
-  return (
-    <div className="main">
-        <div className="nav">
-            <p id='title'>AtomiQ</p>
-            <img src={assets.user_icon} alt="" srcset="" />
-        </div>
-        <div className="main-container">
-            <div className="greet">
-                <p><span>Hello</span></p>
-                <p>How Can I help you today</p>
-            </div>
-            <div className="cards">
-                <div className="card">
-                    <p>Suggest me a material to use for professional jewelary coating</p>
-                    <img src={assets.compass_icon} alt="" srcset="" />
-                </div>
-                <div className="card">
-                    <p>Give me some ideas to innovate the carbon usage in construction</p>
-                    <img src={assets.bulb_icon} alt="" srcset="" />
-                </div>
-                <div className="card">
-                    <p>Give me some ways to have better coating than with Galvanisation</p>
-                    <img src={assets.message_icon} alt="" srcset="" />
-                </div>
-                <div className="card">
-                    <p>Give me some ways to have better coating than with Galvanisation</p>
-                    <img src={assets.code_icon} alt="" srcset="" />
-                </div>
-            </div>
-            <div className="main-bottom">
-                <div className="search-box">
-                    <input type="text" placeholder='Enter a prompt'/>
-                    <div>
-                        <img src={assets.gallery_icon} alt="" srcset="" />
-                        <img src={assets.mic_icon} alt="" srcset="" />
-                        <img src={assets.send_icon} alt="" srcset="" />
+    const {onSent, recentPrompt, showResult, loading, resultData, setInput, input} = useContext(Context);
+    const resultRef = useRef(null);
+    const [rows, setRows] = useState(1);
+
+    useEffect(() => {
+        const updateRows = () => {
+            if (window.innerWidth <= 600) {
+                setRows(2);
+            } else {
+                setRows(1);
+            }
+        };
+
+        updateRows();
+        window.addEventListener('resize', updateRows);
+        return () => window.removeEventListener('resize', updateRows);
+    }, []);
+
+    useEffect(() => {
+        if (resultRef.current) {
+            resultRef.current.scrollTop = resultRef.current.scrollHeight;
+        }
+    }, [resultData]);
+
+    return (
+        <main className="main">
+            <nav  className="nav">
+                <p onClick={() => newChat()} id='title'>AtomiQ</p>
+                <img src={assets.user_icon} alt=""/>
+            </nav>
+            <div className="main-container">
+
+                {!showResult
+                    ? <>
+                        <div className="greet">
+                            <p><span>Hello</span></p>
+                            <p>How can I help you today?</p>
+                        </div>
+                        <div className="cards">
+                            <div className="card"
+                                 onClick={() => setInput("What are the latest trends in patents for hybrid vehicle engines?")}>
+                                <p>What are the latest trends in patents for hybrid vehicle engines?</p>
+                                <img src={assets.compass_icon} alt=""/>
+                            </div>
+                            <div className="card"
+                                 onClick={() => setInput("List patents related to robotic arms used in manufacturing")}>
+                                <p>List patents related to robotic arms used in manufacturing</p>
+                                <img src={assets.bulb_icon} alt=""/>
+                            </div>
+                            <div className="card"
+                                 onClick={() => setInput("Compare recent patents on hydraulic systems for industrial applications.")}>
+                                <p>Compare recent patents on hydraulic systems for industrial applications.</p>
+                                <img src={assets.message_icon} alt=""/>
+                            </div>
+                            <div className="card" onClick={() => setInput("Identify patents on energy-efficient HVAC systems for industrial use")}>
+                                <p>Identify patents on energy-efficient HVAC systems for industrial use</p>
+                                <img src={assets.code_icon} alt=""/>
+                            </div>
+                        </div>
+                    </>
+                    :
+                    <div className='result' ref={resultRef}>
+                        <div className="result-title">
+                            <img src={assets.user_icon} alt=""/>
+                            <p>{recentPrompt}</p>
+                        </div>
+                        <div className="result-data">
+                            <img className="result-data-icon" src={assets.atomiQ} alt=""/>
+                            {loading ?
+                                <div className='loader'>
+                                    <hr/>
+                                    <hr/>
+                                    <hr/>
+                                </div>
+                                :
+                                <p dangerouslySetInnerHTML={{__html: resultData}}></p>
+                            }
+                        </div>
                     </div>
+                }
+                <div className="main-bottom">
+                    <div className="search-box">
+                        <textarea rows={rows} onChange={(e) => setInput(e.target.value)}
+                                  onKeyUp={(e) => {
+                                      if (e.key === 'Enter') {
+                                          onSent();
+                                      }
+                                  }}
+                                  value={input}
+                                  type="text"
+                                  placeholder="Enter a prompt here"
+                        />
+                        <div className="icon-container">
+                            <button><img src={assets.gallery_icon} alt=""/></button>
+                            <button><img src={assets.mic_icon} alt=""/></button>
+                            <button type="submit" onClick={() => onSent()}><img src={assets.send_icon} alt=""/></button>
+                        </div>
+                    </div>
+                    <p className="bottom-info">
+                        Responses are generated based on the given prompt. Please do not share any personal information.
+                        
+                    </p>
                 </div>
-                <p className="bottom-info"> Information displayed it a result of training the data available </p>
             </div>
-        </div>
-    </div>
-  )
+        </main>
+    );
 }
 
-export default Main
+export default Main;
--- src/components/SideBar/SideBar.jsx
+++ src/components/SideBar/SideBar.jsx
@@ -1,47 +1,58 @@
-import React from 'react'
-import './SideBar.css'
-import { assets } from '../../assets/assets'
+import React, {useContext, useState} from 'react';
+import './Sidebar.css';
+import {assets} from '../../assets/assets';
+import {Context} from '../../context/context';
 
+const Sidebar = () => {
+    const [extended, setExtended] = useState(false);
+    const {onSent, prevPrompts, setRecentPrompt, newChat} = useContext(Context);
 
-
-const SideBar = () => {
-
-    const [extended, setExtended] = React.useState(false)
-
-    
-  return (
-    <div className="sidebar" >
-        <div className="top">
-            <img onClick={()=>setExtended(prev=>!prev)} className='menu' src={assets.menu_icon} alt="" srcset="" />
-            <div className="newChat">
-                <img src={assets.plus_icon} alt="" srcset="" />
-                {extended?<p>New Chat</p>:null}
-            </div>
-            {extended?            <div className="recent">
-                <p className="recent-title">Recent</p>
-                <div className="recent-entry">
-                    <img src={assets.message_icon} alt="" srcset="" />
-                    <p>Recent1</p>
+    const loadPrompt = async (prompt) => {
+        setRecentPrompt(prompt)
+        await onSent(prompt)
+    }
+    return (
+        <aside className={`sidebar ${extended ? 'extended' : 'collapsed'}`}>
+            <div className={`top  ${extended ? '' : 'centered'}`}>
+                <div className="menu" onClick={() => setExtended(prev => !prev)}>
+                    <img src={assets.menu_icon} alt="Menu Icon"/>
                 </div>
-            </div>:null}
+                <div onClick={() => newChat()} className="new-chat">
+                    <img src={assets.plus_icon} alt="Plus Icon"/>
+                    <p className={`${extended ? 'block' : 'none'}`}>New Chat</p>
+                </div>
+                {extended ?
+                    <div className="recent">
+                        <p className="recent-title">Recent</p>
+                        {prevPrompts.map((item, index) => {
+                            return (
+                                <div onClick={() => loadPrompt(item)} className="recent-entry">
+                                    <img src={assets.message_icon} alt=""/>
+                                    <p className="recent-entry-p">{item.slice(0, 18)} ...</p>
+                                </div>
+                            )
+                        })}
 
-        </div>
-        <div className="bottom">
-            <div className="bottom-item recent-entry">
-                <img src={assets.question_icon} alt="" srcset="" />
-                {extended?<p>Help</p>:null}
+                    </div>
+                    : null
+                }
             </div>
-            <div className="bottom-item recent-entry">
-                <img src={assets.history_icon} alt="" srcset="" />
-                {extended?<p>Activity</p>:null}
+            <div className={`bottom  ${extended ? '' : 'centered'}`}>
+                <div className="bottom-item recent-entry">
+                    <img src={assets.question_icon} alt="Question Icon"/>
+                    <p className={`fade ${extended ? 'block' : 'none'}`}>Help</p>
+                </div>
+                <div className="bottom-item recent-entry">
+                    <img src={assets.history_icon} alt="History Icon"/>
+                    <p className={`fade ${extended ? 'block' : 'none'}`}>Activity</p>
+                </div>
+                <div className="bottom-item recent-entry">
+                    <img src={assets.setting_icon} alt="Settings Icon"/>
+                    <p className={`fade ${extended ? 'block' : 'none'}`}>Settings</p>
+                </div>
             </div>
-            <div className="bottom-item recent-entry">
-                <img src={assets.setting_icon} alt="" srcset="" />
-                {extended?<p>Settings</p>:null}
-            </div>
-        </div>
-    </div>
-  )
+        </aside>
+    );
 }
 
-export default SideBar
+export default Sidebar;
